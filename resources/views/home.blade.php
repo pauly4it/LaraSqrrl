f7df6ab981167bb7735ea87e9d55fa290b590579
Paul Foryt <paulyf@gmail.com>
2015-11-09 23:05:35 -0800

    Rename welcome view and create preliminary landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,12 +33,30 @@
             .title {
                 font-size: 96px;
             }
+            
+            .tagline {
+                font-size: 60px;
+                padding-top: 20px;
+            }
+
+            .hook {
+                font-size: 40px;
+                padding-top: 18px;
+            }
+
+            .join {
+                font-size: 30px;
+                padding-top: 15px;
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">LaraSqrrl</div>
+                <div class="tagline">The best squirrel tracker. EVER.</div>
+                <div class="hook">Ready to start?</div>
+                <div class="join">Text "Join" to +1-678-64-SQRRL (77775)</div>
             </div>
         </div>
     </body>
